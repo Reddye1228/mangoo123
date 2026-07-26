e423904faf5e4cb4c485e26886795b126f527443
stini <stini@ESPKREDDY>
2026-07-26 16:55:23 +0530

    excute process

---
+++ src/test/java/project2/whatsapp.java
@@ -0,0 +1,19 @@
+package project2;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class whatsapp {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
